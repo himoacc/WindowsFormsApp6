1d9c1f23d3a05daab84592856c302b5503b1a35b
himo_acc <himo_acc@LAPTOP-N4QDN8AP>
2023-04-28 15:09:55 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,194 @@
+﻿
+namespace WindowsFormsApp6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.show = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.txtmedicine = new System.Windows.Forms.TextBox();
+            this.txtprice = new System.Windows.Forms.TextBox();
+            this.txtexp_date = new System.Windows.Forms.TextBox();
+            this.txtquantity = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.insert = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // show
+            // 
+            this.show.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.show.Location = new System.Drawing.Point(704, 408);
+            this.show.Name = "show";
+            this.show.Size = new System.Drawing.Size(84, 30);
+            this.show.TabIndex = 0;
+            this.show.Text = "show";
+            this.show.UseVisualStyleBackColor = false;
+            this.show.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.Black;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(-2, 138);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 26;
+            this.dataGridView1.Size = new System.Drawing.Size(519, 321);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // txtmedicine
+            // 
+            this.txtmedicine.Location = new System.Drawing.Point(12, 85);
+            this.txtmedicine.Name = "txtmedicine";
+            this.txtmedicine.Size = new System.Drawing.Size(119, 24);
+            this.txtmedicine.TabIndex = 2;
+            // 
+            // txtprice
+            // 
+            this.txtprice.Location = new System.Drawing.Point(241, 85);
+            this.txtprice.Name = "txtprice";
+            this.txtprice.Size = new System.Drawing.Size(111, 24);
+            this.txtprice.TabIndex = 3;
+            // 
+            // txtexp_date
+            // 
+            this.txtexp_date.Location = new System.Drawing.Point(457, 85);
+            this.txtexp_date.Name = "txtexp_date";
+            this.txtexp_date.Size = new System.Drawing.Size(128, 24);
+            this.txtexp_date.TabIndex = 4;
+            // 
+            // txtquantity
+            // 
+            this.txtquantity.Location = new System.Drawing.Point(657, 85);
+            this.txtquantity.Name = "txtquantity";
+            this.txtquantity.Size = new System.Drawing.Size(131, 24);
+            this.txtquantity.TabIndex = 5;
+            this.txtquantity.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label1.Location = new System.Drawing.Point(44, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 17);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "medicine";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label2.Location = new System.Drawing.Point(257, 37);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 17);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "price";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label3.Location = new System.Drawing.Point(494, 37);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(66, 17);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "exp_date";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label4.Location = new System.Drawing.Point(690, 37);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(59, 17);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "quantity";
+            // 
+            // insert
+            // 
+            this.insert.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.insert.Location = new System.Drawing.Point(693, 138);
+            this.insert.Name = "insert";
+            this.insert.Size = new System.Drawing.Size(84, 30);
+            this.insert.TabIndex = 10;
+            this.insert.Text = "insert";
+            this.insert.UseVisualStyleBackColor = false;
+            this.insert.Click += new System.EventHandler(this.insert_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.AutoValidate = System.Windows.Forms.AutoValidate.EnableAllowFocusChange;
+            this.BackColor = System.Drawing.Color.Black;
+            this.CancelButton = this.show;
+            this.ClientSize = new System.Drawing.Size(823, 450);
+            this.Controls.Add(this.insert);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtquantity);
+            this.Controls.Add(this.txtexp_date);
+            this.Controls.Add(this.txtprice);
+            this.Controls.Add(this.txtmedicine);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.show);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button show;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox txtmedicine;
+        private System.Windows.Forms.TextBox txtprice;
+        private System.Windows.Forms.TextBox txtexp_date;
+        private System.Windows.Forms.TextBox txtquantity;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button insert;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace WindowsFormsApp6
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection conn = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\himo_acc\source\repos\WindowsFormsApp6\Database1.mdf;Integrated Security=True");
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+             
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            conn.Open();
+            SqlDataAdapter da = new SqlDataAdapter("select * from medicine", conn);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+            conn.Close();
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void insert_Click(object sender, EventArgs e)
+        {
+            conn.Open();
+            SqlCommand cmd = new SqlCommand("INSERT INTO medicine " +
+              "(medicine, price, exp_date, quantity) " +
+              "VALUES " +
+              "('" + txtmedicine.Text + "', '" + txtprice.Text + "', '" + txtexp_date.Text + "', '" + txtquantity.Text + "')", conn);
+            cmd.ExecuteNonQuery();
+            conn.Close();
+        }
+    }
+}
